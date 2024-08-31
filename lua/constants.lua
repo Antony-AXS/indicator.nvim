d854040ac8706e9e2a861896985e2c3e5ecca84c
Antony Xavier <antonyaxs324@gmail.com>
2024-08-31 14:44:55 +0530

    Update constants

--- lua/constants.lua
+++ lua/constants.lua
@@ -2,9 +2,9 @@ return {
 	cache = {},
 	autocmd_id = nil,
 	open_win_count = 0,
-	win_hilght_acmd_id = nil,
-	indicator_notify = false,
-	window_notify = false,
 	window_timer = 300,
 	indicator_timer = 1500,
+	window_notify = false,
+	indicator_notify = false,
+	win_hilght_acmd_id = nil,
 }
